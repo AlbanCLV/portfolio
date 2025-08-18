869f20ca0bff9f33093e45e7d12cc66933de6850
AlbanCLV <albancalvo@gmail.com>
2025-08-18 10:58:59 +0200

    Updates

---
+++ precache-manifest.1735d9622aa495519182064a30d92988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a50aeac5094abc4c34676df2e985d3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88139c87d76c8539db12",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d41ee407adc1306205e0",
+    "url": "/static/css/main.1732404d.chunk.css"
+  },
+  {
+    "revision": "88139c87d76c8539db12",
+    "url": "/static/js/2.0d55f810.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.0d55f810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41ee407adc1306205e0",
+    "url": "/static/js/main.ef14ffb7.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
