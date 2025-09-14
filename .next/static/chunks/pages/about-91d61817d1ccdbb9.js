916736d93cf83f143b792e35ac508f625319935b
AlbanCLV <albancalvo@gmail.com>
2025-09-14 14:44:03 +0200

    delete all files

---
+++ .next/static/chunks/pages/about-91d61817d1ccdbb9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[239],{5596:(u,_,n)=>{"use strict";n.r(_),n.d(_,{default:()=>t});var e=n(7876);function t(){return(0,e.jsx)("div",{children:"About Page"})}},6022:(u,_,n)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return n(5596)}])}},u=>{u.O(0,[636,593,792],()=>u(u.s=6022)),_N_E=u.O()}]);
