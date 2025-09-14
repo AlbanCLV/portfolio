278c8f02fe96b41b3418a96e2316054eada61fa0
AlbanCLV <albancalvo@gmail.com>
2025-09-14 15:26:31 +0200

    build modif

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  output: "export",                // nouvelle syntaxe
+  output: 'export',
   images: { unoptimized: true },
-  basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio/" : "",
-  trailingSlash: true,
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
 };
+
+"scripts": {
+  "dev": "next dev",
+  "build": "next build",
+  "predeploy": "npm run build",
+  "deploy": "gh-pages -d out"
+}
