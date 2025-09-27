70e1be097301972307f8a03025496d3ab8cd537c
AlbanCLV <albancalvo@gmail.com>
2025-09-27 11:53:06 +0200

    Updates

---
+++ precache-manifest.1b1bf78a12076f5c56d26bb8d1dc7b8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7899d091e971b50f9585d374b3a119",
+    "url": "/portfolio-english/index.html"
+  },
+  {
+    "revision": "a77d793f6e9e2c7a0003",
+    "url": "/portfolio-english/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "acfb1490070a0198ca59",
+    "url": "/portfolio-english/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "a77d793f6e9e2c7a0003",
+    "url": "/portfolio-english/static/js/2.ca59751e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio-english/static/js/2.ca59751e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acfb1490070a0198ca59",
+    "url": "/portfolio-english/static/js/main.079e94cb.chunk.js"
+  },
+  {
+    "revision": "743a5ea93069a88e6bbd",
+    "url": "/portfolio-english/static/js/runtime-main.592a3cf5.js"
+  }
+]);
